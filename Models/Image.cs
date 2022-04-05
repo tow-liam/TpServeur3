e3ae8ee82c7e2327d4c1732e4eb5ffe7cd40b120
Liam McKeown <liam.mckeown@towsoft.ca>
2022-04-05 10:10:21 -0400

    Ajout identity

---
+++ Models/Image.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TpServeur3.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int ProduitId { get; set; }
+        public Produit Produit { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -35,10 +35,28 @@ namespace TpServeur3
             services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI()
             .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Configuration des paramètres du mot de passe.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                // Configuration du verrouillage de compte.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+                // Configuration des paramètres utilisateur.
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -51,11 +69,12 @@ namespace TpServeur3
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            CreateRoles(services).Wait();
             app.UseAuthentication();
             app.UseAuthorization();
 
@@ -66,6 +85,34 @@ namespace TpServeur3
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roleNames = { "Administrateur", "Client" };
+            IdentityResult roleResult;
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+            IdentityUser user = await UserManager.FindByEmailAsync("courrielTI@cstjean.qc.ca");
+            if (user == null)
+            {
+                user = new IdentityUser()
+                {
+                    UserName = "courrielTI@cstjean.qc.ca",
+                    Email = "courrielTI@cstjean.qc.ca",
+                    SecurityStamp = Guid.NewGuid().ToString("D"),
+                };
+                await UserManager.CreateAsync(user, "179P@ssw0rd");
+            }
+            await UserManager.AddToRoleAsync(user, "Administrateur");
         }
     }
 }
